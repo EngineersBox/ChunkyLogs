9ee5534db40480bf75ca5a30e072f6646a76c488
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2022-03-20 21:51:16 +1100

    Fixed compilation issues, restructured FS, implemented logging and implemented config properties loading

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
-use std::fs::{File, OpenOptions};
-use std::fs;
-use std::sync::Mutex;
-
-use lazy_static::lazy_static;
-use slog::{Drain, Duplicate, Fuse, Logger};
-use slog_async::{Async, OverflowStrategy};
-use slog_json::Json;
-use slog_term::{FullFormat, TermDecorator};
-
 mod compression;
 mod data;
 mod cache;
 mod logging;
 mod configuration;
 mod macros;
-
-use crate::logging::logging::initialize_logging;
-use crate::configuration::config::Config;
 
 #[macro_use]
 extern crate slog;
@@ -25,6 +12,12 @@ extern crate slog_async;
 extern crate slog_json;
 extern crate lazy_static;
 extern crate regex;
+
+use lazy_static::lazy_static;
+use slog::Logger;
+
+use crate::logging::logging::initialize_logging;
+use crate::configuration::config::Config;
 
 lazy_static! {
     static ref LOGGER: Logger = initialize_logging(String::from("chunky_logs_"));
