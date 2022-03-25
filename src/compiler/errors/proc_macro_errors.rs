390fa02cd486b351e6761be2184534a1b597eb23
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2022-03-26 01:01:05 +1100

    Added typed mappings retrieval and method to get all attribute mappings

--- src/compiler/errors/proc_macro_errors.rs
+++ src/compiler/errors/proc_macro_errors.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+#[derive(Debug)]
 pub struct StructFieldNotFoundError {
     pub struct_name: String,
     pub field_name: String
@@ -12,6 +13,21 @@ impl fmt::Display for StructFieldNotFoundError {
             "Could not find field {} in struct {}",
             self.field_name,
             self.struct_name
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct TypedAttributeRetrievalError {
+    pub message: String,
+}
+
+impl fmt::Display for TypedAttributeRetrievalError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Could not parse attribute into concrete type: {}",
+            self.message,
         )
     }
 }
--- src/compiler/proc_macros.rs
+++ src/compiler/proc_macros.rs
@@ -1,14 +1,21 @@
 #[macro_export]
-macro_rules! reflective_attributes {
+macro_rules! reflective_attributes{
     ($struct_vis_spec:vis struct $name:ident {
         $($(#[$field_attribute:meta])? $field_vis_spec:vis $field_name:ident: $field_type:ty,)*
     }) => {
         $struct_vis_spec struct $name {
             $($field_vis_spec $field_name: $field_type,)*
         }
-
         impl $name {
-            fn get_field_attribute(field_name_prm : &str) -> Result<&'static str, crate::compiler::errors::proc_macro_errors::StructFieldNotFoundError> {
+            pub fn get_field_attribute_mappings() -> std::collections::HashMap<String, String> {
+                return core::convert::From::from([
+                    $((
+                        stringify!($field_name).to_string(),
+                        stringify!($($field_attribute)?).to_string().replace(" ", "")
+                    ),)*
+                ]);
+            }
+            pub fn get_field_attribute(field_name_prm: &str) -> Result<&'static str, crate::compiler::errors::proc_macro_errors::StructFieldNotFoundError> {
                 let fields: Vec<&str> = vec![$(stringify!($field_name,$($field_attribute)?)),*];
                 let mut field_attr: &'static str = "@@FNF@@";
 
@@ -25,6 +32,20 @@ macro_rules! reflective_attributes {
                     })
                 }
                 return Ok(field_attr);
+            }
+            pub fn get_field_attribute_typed<T: std::str::FromStr>(field_name_prm: &str) -> Result<T, crate::compiler::errors::proc_macro_errors::TypedAttributeRetrievalError> {
+                let attr: &'static str = match $name::get_field_attribute(field_name_prm) {
+                    Ok(v) => v,
+                    Err(e) => return Err(crate::compiler::errors::proc_macro_errors::TypedAttributeRetrievalError{
+                        message: e.field_name,
+                    }),
+                };
+                return match attr.parse::<T>() {
+                    Ok(v) => Ok(v),
+                    Err(e) => Err(crate::compiler::errors::proc_macro_errors::TypedAttributeRetrievalError{
+                        message: attr.to_string(),
+                    }),
+                }
             }
         }
     }
